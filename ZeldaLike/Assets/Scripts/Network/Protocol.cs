e5d054e5acc01b65f97abf9b148959d1edc1dc68
charg2dang <charg2dang@gmail.com>
2020-06-20 20:37:26 +0900

    프로토콜 추가. (공격, 로그인, 로그아웃) author : 차지환 email : charg2dang@kakao.com

--- ZeldaLike/Assets/Scripts/Network/Protocol.cs
+++ ZeldaLike/Assets/Scripts/Network/Protocol.cs
@@ -9,15 +9,19 @@ public enum PacketType : sbyte
 	PT_NONE,
 
 	S2C_LOGIN_OK = 1,
+	S2C_LOGIN_FAIL,
 	S2C_MOVE,
 	S2C_ENTER,
 	S2C_LEAVE,
 	S2C_CHAT,
+	S2C_STAT_CHANGE,
 	PT_MAX,
 
 	C2S_LOGIN = 1,
 	C2S_MOVE,
+	C2S_ATTACK,
 	C2S_CHAT,
+	C2S_LOGOUT,
 };
 
 public enum Protocol
@@ -42,7 +46,7 @@ public unsafe struct cs_packet_login
 {
     public PacketHeader header;
 
-    public fixed byte name[(int)Protocol.MAX_ID_LEN];
+    public fixed char name[(int)Protocol.MAX_ID_LEN];
 };
 
 [StructLayout(LayoutKind.Sequential, Pack = 1)]
@@ -54,6 +58,12 @@ public unsafe struct cs_packet_move
 	public UInt32		move_time;
 };
 
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public unsafe struct cs_packet_atttack
+{
+	public PacketHeader header;
+};
+
 
 [StructLayout(LayoutKind.Sequential, Pack = 1)]
 public unsafe struct cs_packet_chat
@@ -61,7 +71,14 @@ public unsafe struct cs_packet_chat
 	public PacketHeader header;
 
 	public	int			id;
-	public fixed byte	chat[(int)Protocol.MAX_CHAT_LEN];
+	public fixed char	chat[(int)Protocol.MAX_CHAT_LEN];
+};
+
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public unsafe struct cs_packet_logout
+{
+	public PacketHeader header;
 };
 
 
@@ -77,6 +94,15 @@ public unsafe struct sc_packet_login_ok // 16
 	public short	level;
 	public int		exp;
 };
+
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+struct sc_packet_login_fail
+{
+	PacketHeader header;
+};
+
+
 [StructLayout(LayoutKind.Sequential, Pack = 1)]
 
 public unsafe struct sc_packet_move
@@ -94,7 +120,7 @@ public unsafe struct sc_packet_enter //
 	public PacketHeader header;
 
 	public int id;
-	public fixed byte name[(int)Protocol.MAX_ID_LEN];
+	public fixed char name[(int)Protocol.MAX_ID_LEN]; // wide-char로 변경 예정.
 	public sbyte o_type;
 	public short x, y;
 };
@@ -115,5 +141,15 @@ public unsafe struct sc_packet_chat
 	public PacketHeader header;
 
 	public int id;
-	public fixed byte chat[(int)Protocol.MAX_CHAT_LEN];
+	public fixed char chat[(int)Protocol.MAX_CHAT_LEN];
+};
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+struct sc_packet_stat_change
+{
+	PacketHeader header;
+
+	short	hp;
+	short	level;
+	int		exp;
 };
